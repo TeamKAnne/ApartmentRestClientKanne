547ad3b06a79fe9d0ba0cd8c111ffdb25e797b11
Anne Sofie Molsted Wanscher <anne55x9@edu.easj.dk>
2017-10-03 13:16:23 +0200

    lort

---
+++ ApartmentRestClientKanne/Apartment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApartmentRestClientKanne
+{
+    public class Apartment
+    {
+        private int id;
+        private int price;
+        private string location;
+        private int postalCode;
+        private int size;
+        private int noRoom;
+        private bool washingMachine;
+        private bool dishwasher;
+
+        public int Id { get => id; set => id = value; }
+        public int Price { get => price; set => price = value; }
+        public string Location { get => location; set => location = value; }
+        public int PostalCode { get => postalCode; set => postalCode = value; }
+        public int Size { get => size; set => size = value; }
+        public int NoRoom { get => noRoom; set => noRoom = value; }
+        public bool WashingMachine { get => washingMachine; set => washingMachine = value; }
+        public bool Dishwasher { get => dishwasher; set => dishwasher = value; }
+
+        public override string ToString()
+        {
+            return Location;
+        }
+    }
+}
--- ApartmentRestClientKanne/Program.cs
+++ ApartmentRestClientKanne/Program.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace ApartmentRestClientKanne
 {
@@ -10,6 +15,23 @@ namespace ApartmentRestClientKanne
     {
         static void Main(string[] args)
         {
+            IList<Apartment> aplist = new List<Apartment>();
+
+            using (var client = new HttpClient())
+            {
+                const string serverurl = "http://apartmentrestkanne20171003122404.azurewebsites.net/ApartmentService.svc/";
+
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                
+                client.BaseAddress = new Uri(serverurl);
+
+
+                Task<HttpResponseMessage> task = client.GetAsync("apartment/");
+
+                Console.WriteLine(task);
+            }
+   
+            Console.ReadLine();
         }
     }
 }
