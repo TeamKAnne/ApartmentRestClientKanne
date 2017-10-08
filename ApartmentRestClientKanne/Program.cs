9aee1f87ec4ae11f491b9c5cd4c21aec3ab12f19
Anne Sofie Molsted Wanscher <anne55x9@edu.easj.dk>
2017-10-08 11:54:29 +0200

    Tilføjet to clienter mere som kalder sorteringsmetoderne. Det er requeststr variable som ændre sig.

--- ApartmentRestClientKanne/Program.cs
+++ ApartmentRestClientKanne/Program.cs
@@ -32,12 +32,72 @@ namespace ApartmentRestClientKanne
                 if (responseAsync.IsSuccessStatusCode)
                 {
                     aplist = (List<Apartment>)responseAsync.Content.ReadAsAsync<IList<Apartment>>().Result;
+
+                    //aplist = responseAsync.Content.ReadAsAsync<IList<Apartment>>().Result as List<Apartment>;
+
                     
                     foreach (Apartment a in aplist)
                     {
                         Console.WriteLine();
                         Console.WriteLine("Lejlighed til salg:");
                         
+                        Console.WriteLine(a.ToString());
+                    }
+                }
+            }
+
+            using (var client = new HttpClient())
+            {
+                const string serverurl =
+                    "http://apartmentrestkanne20171003122404.azurewebsites.net/ApartmentService.svc/";
+
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                client.BaseAddress = new Uri(serverurl);
+
+                string requeststr = "apartment/postalcode/{code}";
+
+                var responseAsyncPC = client.GetAsync(serverurl + requeststr).Result;
+
+                if (responseAsyncPC.IsSuccessStatusCode)
+                {
+                    aplist = (List<Apartment>) responseAsyncPC.Content.ReadAsAsync<IList<Apartment>>().Result;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Sortering på postnummer:");
+
+                foreach (Apartment a in aplist)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(a.ToString());
+                }
+            }
+
+
+            using (var client = new HttpClient())
+            {
+                const string serverurl =
+                    "http://apartmentrestkanne20171003122404.azurewebsites.net/ApartmentService.svc/";
+
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                client.BaseAddress = new Uri(serverurl);
+
+                string requeststr = "apartment/location/{location}";
+
+                var responseAsyncLocation = client.GetAsync(serverurl + requeststr).Result;
+
+                if (responseAsyncLocation.IsSuccessStatusCode)
+                {
+                    aplist = (List<Apartment>) responseAsyncLocation.Content.ReadAsAsync<IList<Apartment>>().Result;
+
+                    Console.WriteLine();
+                    Console.WriteLine("Sortering på sted (Å er pt et problem):");
+
+                    foreach (Apartment a in aplist)
+                    {
+                        Console.WriteLine();
                         Console.WriteLine(a.ToString());
                     }
                 }
